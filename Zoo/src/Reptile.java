ba19671a3f6850fc8679753f221c5da2b57c2b2c
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2023-12-28 18:54:00 -0500

    Create Reptile.java

---
+++ Zoo/src/Reptile.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Reptile implements Comparable<Reptile>{
+
+    @Override
+    public int compareTo(Reptile r) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+        //return this.name.compareTo(r.name);
+        //return this.name.compareTo(c.name);  <<--- should look like this? 
+
+    }
+
+}
