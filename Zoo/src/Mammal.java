18f2c281f9990685ed404b0c731d53a42da82aed
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2023-12-28 18:53:12 -0500

    Create Mammal.java

---
+++ Zoo/src/Mammal.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Mammal implements Comparable<Mammal> {
+
+    @Override
+    public int compareTo(Mammal arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    }
+    
+}
