ed0a5bbc4d0d51a3fd1cfda4474e2033166a2ac3
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2023-12-28 18:52:19 -0500

    Create Fish.java

---
+++ Zoo/src/Fish.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Fish implements Comparable<Fish>{
+
+    @Override
+    public int compareTo(Fish arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    }
+    
+}
