fc63139776392084e45356294c4b8229265265c8
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2023-12-28 18:51:46 -0500

    Create Bird.java

---
+++ Zoo/src/Bird.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Bird implements Comparable<Bird>{
+
+    @Override
+    public int compareTo(Bird arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    }
+    
+
+}
